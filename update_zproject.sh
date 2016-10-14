596e6a7faafc8d75cb5f8b041b2da3318e182b0a
mrjnumber1 <multicoloredfunk@hotmail.com>
2016-10-14 09:11:51 -0500

    updated some dumb things i wrotels

--- update_zproject.sh
+++ update_zproject.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 
-command -v cd ../zproject 
-command -v make >/dev/null 2>&1 
-command -v make install >/dev/null 2>&1
+command cd ../zproject 
+command pwd
+command make 
+command make install 
 
 exit 1
 
